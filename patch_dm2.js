e1fe2ce83a24e447e03f3708a0d4822a883e0ad7
dlolis <dlolis88@gmail.com>
2026-09-23 20:44:03 +0300

    chore: remove temporary patch scripts

--- patch_dm2.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-let code = fs.readFileSync('client/src/app/features/dm/dm.component.ts', 'utf8');
-
-code = code.replace(
-  'passive_perception: passivePerception(stats),',
-  'passive_perception: 10,'
-);
-
-fs.writeFileSync('client/src/app/features/dm/dm.component.ts', code);
